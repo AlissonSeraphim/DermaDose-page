6ef9b265bc86dd25e7a4f6c02d203ce7f2de6eb5
AlissonSeraphim <projetoseraphim@gmail.com>
2023-06-10 04:57:59 -0300

    feat: mission, vision and values from company

---
+++ src/components/Values.jsx
@@ -0,0 +1,108 @@
+export default function Values() {
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="text-center mb-20">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
+            Por que fazemos o que fazemos!
+          </h1>
+          <div class="flex mt-6 justify-center">
+            <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+          </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                enable-background="new 0 0 24 24"
+                viewBox="0 0 24 24"
+                id="mission"
+              >
+                <path
+                  d="M22,3h-1V2c0-0.5527-0.4478-1-1-1s-1,0.4473-1,1v1.5175c-0.5538,0.4134-1.0881,0.8461-1.6071,1.2943
+		C15.8878,3.6797,14.024,3,12,3c-4.9624,0-9,4.0371-9,9s4.0376,9,9,9s9-4.0371,9-9c0-2.0241-0.6797-3.8879-1.8118-5.3929
+		C19.6364,6.0881,20.0691,5.5538,20.4825,5H22c0.5522,0,1-0.4473,1-1S22.5522,3,22,3z M19,12c0,3.8594-3.1401,7-7,7s-7-3.1406-7-7
+		s3.1401-7,7-7c1.4357,0,2.7709,0.4357,3.8829,1.1801c-0.2909,0.2767-0.5802,0.555-0.8662,0.8366
+		c-0.2061,0.2069-0.4081,0.4178-0.6114,0.6274C13.6884,7.2464,12.8763,7,12,7c-2.7568,0-5,2.2432-5,5s2.2432,5,5,5s5-2.2432,5-5
+		c0-0.8763-0.2464-1.6884-0.6442-2.4053c0.2097-0.2033,0.4206-0.4053,0.6274-0.6114c0.2816-0.286,0.5599-0.5753,0.8366-0.8663
+		C18.5643,9.2291,19,10.5644,19,12z M15,12c0,1.6543-1.3457,3-3,3s-3-1.3457-3-3s1.3457-3,3-3c0.3458,0,0.6729,0.0706,0.9823,0.1787
+		c-0.5925,0.6754-1.1614,1.3744-1.6894,2.1142c-0.2436,0.3425-0.2579,0.8166,0,1.1779c0.3253,0.4557,0.9584,0.5615,1.4142,0.2363
+		c0.7398-0.528,1.4388-1.0969,2.1142-1.6894C14.9294,11.3271,15,11.6542,15,12z"
+                ></path>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                Missão
+              </h2>
+              <p class="leading-relaxed text-base">
+                Liderar o mercado de tratamento contra diabetes, através de
+                tecnologia acessível para gerenciamento e evolução de pacientes
+                e profissionais da saúde.
+              </p>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 64 64"
+                id="vision"
+              >
+                <path d="M60,31A28,28,0,0,0,33,4c-.33,0-.66,0-1,0s-.67,0-1,0A28,28,0,0,0,4,31c0,.33,0,.66,0,1s0,.67,0,1A28,28,0,0,0,31,60c.33,0,.66,0,1,0s.67,0,1,0A28,28,0,0,0,60,33c0-.33,0-.66,0-1S60,31.33,60,31ZM33,58l-1,0-1,0A26,26,0,0,1,6,33c0-.33,0-.67,0-1s0-.67,0-1A26,26,0,0,1,31,6l1,0,1,0A26,26,0,0,1,58,31c0,.33,0,.67,0,1s0,.67,0,1A26,26,0,0,1,33,58Z"></path>
+                <path d="M32,11A21,21,0,1,0,53,32,21,21,0,0,0,32,11Zm0,40A19,19,0,1,1,51,32,19,19,0,0,1,32,51Z"></path>
+                <path
+                  d="M32,13A19,19,0,1,0,51,32,19,19,0,0,0,32,13Zm0,30A19.56,19.56,0,0,1,14.61,32.46a.94.94,0,0,1,0-.92,19.62,19.62,0,0,1,34.78,0,.94.94,0,0,1,0,.92A19.56,19.56,0,0,1,32,43Z"
+                  opacity=".5"
+                ></path>
+                <path d="M49.39 31.54a19.62 19.62 0 0 0-34.78 0 .94.94 0 0 0 0 .92 19.62 19.62 0 0 0 34.78 0A.94.94 0 0 0 49.39 31.54zM32 41a17.56 17.56 0 0 1-15.37-9 17.62 17.62 0 0 1 30.74 0A17.56 17.56 0 0 1 32 41zM32 9a1 1 0 0 1-1-1V2a1 1 0 0 1 2 0V8A1 1 0 0 1 32 9zM32 63a1 1 0 0 1-1-1V56a1 1 0 0 1 2 0v6A1 1 0 0 1 32 63zM8 33H2a1 1 0 0 1 0-2H8a1 1 0 0 1 0 2zM62 33H56a1 1 0 0 1 0-2h6a1 1 0 0 1 0 2z"></path>
+                <path d="M32,24a8,8,0,1,0,8,8A8,8,0,0,0,32,24Zm0,14a6,6,0,1,1,6-6A6,6,0,0,1,32,38Z"></path>
+                <path
+                  d="M32,26a6,6,0,1,0,6,6A6,6,0,0,0,32,26Zm0,10a4,4,0,1,1,4-4A4,4,0,0,1,32,36Z"
+                  opacity=".5"
+                ></path>
+                <path d="M32,28a4,4,0,1,0,4,4A4,4,0,0,0,32,28Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,32,34Z"></path>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                Visão
+              </h2>
+              <p class="leading-relaxed text-base">
+                Melhorar a qualidade de vida dos usuários através de pequenas
+                coisas, como um tratamento mais saudável, confortável e
+                acessível.
+              </p>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 64 64"
+                id="value"
+              >
+                <path
+                  d="m55.38 13-8-8H16.59l-8 8L32 38.48ZM32 14h6.61L32 33.84 25.39 14Zm-5.34-2L32 7.33 37.34 12H26.66Zm14.06 2h11L34.43 32.87Zm1.69-2L47 7.41 51.59 12ZM40 11.63 34.66 7h9.93Zm-15.9 0L19.41 7h9.93Zm-2.51.37h-9.18L17 7.41Zm1.69 2 6.29 18.87L12.27 14ZM56 45.57c-1.52-1.5-4.22-1.51-4.51-1.5L36 44a5.68 5.68 0 0 0-1.59-3.59A5.05 5.05 0 0 0 31 39H20a7.87 7.87 0 0 0-6.38 3H7v17h22.23C42.69 59 57 55.14 57 48a3.34 3.34 0 0 0-1-2.43ZM9 57V44h3.9l.1 13Zm20.23 0H15V43.5a5.84 5.84 0 0 1 5-2.5h11a3 3 0 0 1 1.94.83 3.65 3.65 0 0 1 1 2.17H26v2h8.93l16.57.07a5.16 5.16 0 0 1 3.1.92 1.34 1.34 0 0 1 .4 1C55 52.88 43.2 57 29.23 57Z"
+                  data-name="30-Value"
+                ></path>
+              </svg>
+            </div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                Valores
+              </h2>
+              <p class="leading-relaxed text-base">
+                Valorizar a dignidade, igualdade e respeito a todas as pessoas,
+                superar as expectativas dos clientes e buscar excelência em
+                parcerias. Acreditar no potencial das pessoas, promovendo seu
+                desenvolvimento profissional e pessoal.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero';
 import LogoCloud from '../components/LogoCloud';
+import Values from '../components/Values';
 
 function MainPage() {
   return (
     <div>
       <Hero />
+      <Values />
       <LogoCloud />
       <h1>main page</h1>
     </div>
